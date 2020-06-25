31373b15d45ba4c021aa20c4f5f101bb3610baf5
Myke <16993755+MykeeBee@users.noreply.github.com>
2020-06-25 15:57:46 +0100

    Add project files.

---
+++ RomanNumeralParser/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+//upload?
+
+namespace RomanNumeralParser
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Numerals = new Dictionary<char, int>();
+            {
+                Numerals.Add('I', 1);
+                Numerals.Add('V', 5);
+                Numerals.Add('X', 10);
+                Numerals.Add('L', 50);
+                Numerals.Add('C', 100);
+                Numerals.Add('D', 500);
+                Numerals.Add('M', 1000);
+            }
+
+            var romanString = "MMXX";
+            var numeralToInt = 0;
+
+            for (int i = 0; i < romanString.Length; i++)
+            {
+                if (i + 1 < romanString.Length && Numerals[romanString[i]] < Numerals[romanString[i + 1]])
+                {
+                    numeralToInt -= Numerals[romanString[i]];
+                }
+                else
+                {
+                    numeralToInt += Numerals[romanString[i]];
+                }
+            }
+
+            Console.WriteLine(numeralToInt);
+        }
+    }
+}
